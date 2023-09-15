d0622966ba45c073867ed2253005736af39c2d42
Rosanne Zhu <110139243+roskzhu@users.noreply.github.com>
2023-09-14 23:31:20 -0400

    misc

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,35 +1,46 @@
 import { project } from "../constants";
 import styles, { layout } from "../style";
 import ProjectCard from "./ProjectCard";
+import AOS from "aos";
+import "aos/dist/aos.css";
+import React, { useEffect } from "react";
+import Fade from 'react-reveal/Fade';
 
-const Projects = () => (
+const Projects = () => {
+  useEffect(() => {
+    AOS.init();
+  }, []);
+
+  return (
   <section id="projects" className={`${styles.paddingY} ${styles.flexStart} flex-col relative`}>
     
-    {/* gradient hover */}
-    {/* <div className="absolute z-[0] w-[40%] h-[40%] -right-1/2 rounded-full purple__gradient bottom-40" /> */}
-    <div className="absolute z-[3] -left-1/2 w-[45%] h-[45%] rounded-full purple__gradient" />
-
-        {/* gradient start */}
-        {/* <div className="absolute z-[3] -left-1/2 top-0 w-[40%] h-[40%] rounded-full white__gradient" />
-        <div className="absolute z-[0] w-[40%] h-[40%] -left-1/2 bottom-0 rounded-full pink__gradient" /> */}
-        {/* <div className="absolute z-[0] w-[60%] h-[60%] -left-1/2 bottom-0 rounded-full blue__gradient" /> */}
-        {/* gradient end */}
+      {/* gradients */}
+      {/* <div className="absolute z-[0] w-[40%] h-[40%] -right-1/2 rounded-full purple__gradient bottom-40" /> */}
+      {/* <div className="absolute z-[3] -left-1/2 w-[45%] h-[45%] rounded-full purple__gradient" /> */}
+      {/* <div className="absolute z-[3] -left-1/2 top-0 w-[40%] h-[40%] rounded-full white__gradient" /> */}
     
-    <div className={layout.sectionInfo}>
-      <h2 className={styles.heading2}>
-        Projects
-      </h2>
-      {/* <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
-      Here are some projects I've worked on.
-      </p>   */}
-      <br className="sm:block hidden" /> {" "}
-    </div>
+     <div className={layout.sectionInfo}>
+          <Fade left delay={150}>
+          <h1 className={`${styles.cover} text-white ss:leading-[100px] leading-[74px] w-full text-white text-left mt-5 z-[4]`}>
+          <span className="text-gradient text rounded-3xl italic">
+            Projects
+          </span>
+        </h1>
+        </Fade>
+          </div>
 
-    <div className="flex flex-wrap sm:justify-start justify-items-center w-full project-container relative z-[1]">
-      {project.map((card) => <ProjectCard key={card.id} {...card} />)}
+    <div className="flex flex-wrap sm:justify-start justify-items-center w-full 
+    project-container relative z-[1]">
+      {project.map((card, i) => 
+                    <FadeInSection delay={`${i + 1}00ms`}>
+      <ProjectCard key={card.id} {...card} />
+      </FadeInSection>
+
+      )}
     </div>
   </section>
-);
+  );
+};
 
 
 export default Projects;
--- src/constants/index.js
+++ src/constants/index.js
@@ -7,15 +7,7 @@ export const navLinks = [
   },
   {
     id: "projects",
-    title: "PROJECTS",
-  },
-    {
-    id: "about",
-    title: "ABOUT",
-  },
-  {
-    id: "contact",
-    title: "CONTACT",
+    title: "MY WORK",
   },
 ];
 
@@ -34,49 +26,25 @@ export const project = [
     id: "project-1",
     name: "iSpy",
     content:
-      "A web application that recognizes faces with a live camera feed with up to a 95% accuracy rate. Written with Python using OpenCV, and Tensorflow libraries. Frontend is made with vanilla Javascript and HTML/CSS. ",
-    // title: "Languages and Tools",
+      "Facial recognition web application.",
+    title: "Python // OpenCV // Tensorflow // Javascript",
+    img: ispy,
+  },
+  {
+    id: "project-3",
+    content:
+      "Tetris variation, expanded for 2-player competition.",
+    name: "Tetris",
+    title: "C++",
     img: ispy,
   },
   {
     id: "project-2",
     content:
-      "A full-stack web application recipe making AI where the user takes a picture of their open fridge and uploads it to the website. The machine learning AI then automatically detects what items are in the fridge and outputs them onto an ingredient list. Using this data, another AI produces all possible recipes that can be made with the ingredients.",
+      "Full-stack recipe making AI.",
     name: "Fridge Smart",
-    // title: "Languages and Tools",
-    img: fridges,
-  },
-  {
-    id: "project-3",
-    content:
-      "Given a large set of CSV medical documents, I used NLTK libraries to tokenize and stem and train large data sets. Scripts were written in Python to automatically scrape and update medical documents, classifying medical transcripts to specialties, using Pandas and NumPy libraries.",
-    name: "CxC Data Science Challenge",
-    // title: "Languages and Tools",
-    img: cxc,
-  },
-  {
-    id: "project-4",
-    content:
-      "A web application built using React.js that visualizes and compares different sorting algorithms, such as insertion sort, merge sort, bubble sort, and quick sort. ",
-    name: "Sorting Algorithm Visualizer",
-    // title: "Languages and Tools",
-    img: sort,
-  },
-  {
-    id: "project-5",
-    content:
-      "A sudoku solver engine that solves a sudoku board using recursion and backtracking algorithms, coded in Python.",
-    name: "Sudoku Solver",
-    // title: "Languages and Tools",
-    img: sudoku,
-  },
-  {
-    id: "project-6",
-    content:
-      "This is my website! Check out the source code here. Built using React.js.",
-    name: "Personal Portfolio",
-    // title: "Languages and Tools",
-    img: portfolio,
+    title: "Javascript // YOLOv8",
+    img: ispy,
   },
 ];
 
